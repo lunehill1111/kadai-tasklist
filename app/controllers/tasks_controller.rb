73e9d613369e54e86eca22a55ff6152e2550fd3f
lunehill1111 <tacktown@outlook.jp>
2020-12-25 00:50:05 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  def index
+     @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+     @tasks = Task.new(tasklist_params)
+
+    if @tasks.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+        @tasks = Task.find(params[:id])
+
+    if @tasks.update(message_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+        @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+end
+
+  def tasklist_params
+    params.require(:tasks).permit(:content)
+  end
