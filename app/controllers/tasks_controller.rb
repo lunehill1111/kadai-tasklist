5a60c6f6b101d176c9990f3d4d14cbb397702b42
lunehill1111 <tacktown@outlook.jp>
2021-01-09 05:24:05 +0000

    login3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :create, :new, :update, :destoroy]
+  before_action :require_user_logged_in, only: [:index, :create, :new, :update, :destoroy, :show]
   
   def index
  
