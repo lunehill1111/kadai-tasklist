70cfba091c9dda977971a359c27b96b5e7ab3dfa
lunehill1111 <tacktown@outlook.jp>
2021-01-14 08:19:24 +0000

    login4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :create, :new, :update, :destoroy, :show]
+ 
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destoroy, :show]
   
   def index
  
@@ -62,6 +63,13 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
